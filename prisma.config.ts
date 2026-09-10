40a07d638d7aafefb20204be5bfeff79098c401f
Gregory <gregoryperezfigueredo@gmail.com>
2026-09-10 14:05:52 -0400

    Se realizo la configuracion correcta para establecer conexion con mongodb y ademas se corrigio el dockerfile.prod

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-import { envs } from "./src/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -12,6 +11,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: envs.dataBaseUrl,
+    url: process.env["DATABASE_URL"] || '',
   },
 });
